e39dcdddfccbbe773496baedacc8c904e95ba556
easymbol <xingcxb@hotmail.com>
2022-06-05 22:15:08 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,12 @@ echo 'xingcxb.github.io' > CNAME
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+  githubUrl=git@github.com:xingcxb/xingcxb.github.io.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
+  githubUrl=https://xingcxb:${GITHUB_TOKEN}@github.com/xingcxb/xingcxb.github.io.git
+  git config --global user.name "xingcxb"
+  git config --global user.email "xingcxb@hotmail.com"
 fi
 git init
 git add -A
