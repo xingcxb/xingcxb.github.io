4144a980e5a974879bfea0d0f21f0c9731f552e5
symbol <xingcxb@hotmail.com>
2022-06-06 18:05:34 +0800

    update bar

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,10 +1,10 @@
-const experience = require("../nav/experience");
+const go = require("../nav/go");
 const projects = require("../nav/projects");
 module.exports = [
   { text: "首页", link: "/" },
   { text: "吐槽", link: "/speak/" },
-  experience,
-  { text: "读书笔记", link: "/book/" },
+  go,
+  { text: "读书笔记", link: "/read/" },
   projects,
   { text: "时间线", link: "/archives/" },
   { text: "关于", link: "/about/" },
---
+++ docs/.vuepress/nav/go.js
@@ -0,0 +1,23 @@
+// 经验导航栏
+module.exports = {
+  text: "GoLang",
+  link: "/go/",
+  items: [
+    {
+      text: "基础",
+      items: [
+        {
+          text: "基础库",
+          link: "/basic/",
+        },
+      ],
+    },
+    {
+      text: "第三方库",
+      items: [
+        { text: "gjson", link: "/gjson/" },
+        { text: "go-sqlite3", link: "/go-sqlite3/" },
+      ],
+    },
+  ],
+};
