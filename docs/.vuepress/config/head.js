ca91fa722de71da73f76e56d957c0226559e1fea
symbol <xingcxb@hotmail.com>
2022-06-24 10:18:33 +0800

    update ads

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -13,9 +13,9 @@ module.exports = [
   [
     "script",
     {
-      "data-ad-client": "ca-pub-1725717718088510",
-      async: true,
-      src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+      async,
+      crossorigin: "anonymous",
+      src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1725717718088510",
     },
   ],
   [
