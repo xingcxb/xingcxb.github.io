2601df6be87be7ac30e978e741eb471fa6aca55b
symbol <xingcxb@hotmail.com>
2022-06-24 10:23:05 +0800

    fix script

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -13,7 +13,7 @@ module.exports = [
   [
     "script",
     {
-      async,
+      async: "",
       crossorigin: "anonymous",
       src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1725717718088510",
     },
