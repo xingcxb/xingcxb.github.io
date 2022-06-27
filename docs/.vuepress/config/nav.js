bd6c217f42040f28a7c41e64b16b3602e29e1ed7
symbol <xingcxb@hotmail.com>
2022-06-27 15:40:06 +0800

    friend

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,6 +1,8 @@
 const language = require("../nav/language");
 const projects = require("../nav/projects");
 const tools = require("../nav/tools");
+const more = require("../nav/more");
+
 module.exports = [
   { text: "首页", link: "/" },
   { text: "吐槽", link: "/speak/" },
@@ -10,4 +12,5 @@ module.exports = [
   projects,
   { text: "时间线", link: "/archives/" },
   { text: "关于", link: "/about/" },
+  more,
 ];
---
+++ docs/.vuepress/nav/more.js
@@ -0,0 +1,5 @@
+module.exports = {
+  text: "更多",
+  link: "/more/",
+  items: [{ text: "友情链接", link: "/more/friend/8e1a3d/" }],
+};
