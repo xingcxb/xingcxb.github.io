c1b128628452104f201f4ae352d495b49658bc51
symbol <xingcxb@hotmail.com>
2022-07-13 11:30:28 +0800

    。

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -60,9 +60,9 @@ module.exports = [
   [
     "script",
     {
-      "data-ad-client": "ca-pub-1725717718088510",
       async: "async",
-      src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+      src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1725717718088510",
+      crossorigin: "anonymous",
     },
   ], // 网站关联Google AdSense 与 html格式广告支持
 ];
