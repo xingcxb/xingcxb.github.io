0fc285219ed2e72eb1b10a462b67e0638a14329a
symbol <xingcxb@hotmail.com>
2022-07-13 15:10:50 +0800

    .

--- docs/.vuepress/nav/more.js
+++ docs/.vuepress/nav/more.js
@@ -1,5 +1,8 @@
 module.exports = {
   text: "更多",
   link: "/more/",
-  items: [{ text: "友情链接", link: "/more/friend/8e1a3d/" }],
+  items: [
+    { text: "友情链接", link: "/more/friend/8e1a3d/" },
+    { text: "免费信息", link: "/more/free/" },
+  ],
 };
