9dae76dcebf79f428aa810dff948e08997ae95ef
xingcxb <xingcxb@hotmail.com>
2023-02-11 09:50:40 +0800

    .

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -1,6 +1,5 @@
 const baidutj = require("../common/baidutj.js");
 const googletj = require("../common/googletj.js");
-const googleAdblock = require("../common/adblock.js");
 
 // head
 module.exports = [
@@ -68,7 +67,6 @@ module.exports = [
   ],
   // 谷歌统计 js
   ["script", {}, googletj],
-  ["script", {}, googleAdblock],
   [
     "script",
     {
