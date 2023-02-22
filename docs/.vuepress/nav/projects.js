e427713b880b70179dc0434c5ae12aaa96a5efbe
xingcxb <xingcxb@hotmail.com>
2023-02-22 09:30:38 +0800

    .

--- docs/.vuepress/nav/projects.js
+++ docs/.vuepress/nav/projects.js
@@ -2,5 +2,8 @@
 module.exports = {
   text: "项目",
   link: "/projects/",
-  items: [{ text: "ToDb", link: "https://github.com/xingcxb/todb" }],
+  items: [
+    { text: "ToDb", link: "https://github.com/xingcxb/todb" },
+    { text: "goKit", link: "https://github.com/xingcxb/goKit" },
+  ],
 };
