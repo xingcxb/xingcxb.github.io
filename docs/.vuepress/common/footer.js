a7403e649d7274864681d32314755170f7a54aa4
xingcxb <xingcxb@hotmail.com>
2024-02-25 12:13:42 +0800

    。

--- docs/.vuepress/common/footer.js
+++ docs/.vuepress/common/footer.js
@@ -6,5 +6,5 @@ module.exports = {
     penName +
     " | " +
     footerTitle +
-    '<br> <a href="http://beian.miit.gov.cn/" target="_blank"></a><a href="/sitemap.xml">sitemap</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>', // 博客版权信息，支持a标签
+    '<br> <a href="/sitemap.xml">sitemap</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a><br/><a href="https://www.foreverblog.cn/go.html" target="_blank"> <img src="https://img.foreverblog.cn/wormhole_3_tp.gif" alt="" style="width:auto;height:32px;" title="穿梭虫洞-随机访问十年之约友链博客"></a>', // 博客版权信息，支持a标签
 };
