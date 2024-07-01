992ac9d281041fef10b80800c6dbd957486bab1b
xingcxb <xingcxb@hotmail.com>
2024-07-02 00:09:38 +0800

    .

--- docs/.vuepress/nav/more.js
+++ docs/.vuepress/nav/more.js
@@ -3,6 +3,7 @@ module.exports = {
   link: "/more/",
   items: [
     { text: "友情链接", link: "/more/friend/8e1a3d/" },
+    { text: "👻故事", link: "/more/ghost/" },
     { text: "免费信息", link: "/more/free/" },
     { text: "时间线", link: "/archives/" },
     { text: "关于", link: "/about/" },
